d09118e8b924010484725ac9293767f65dbc0818
k1ndrat <rubalkanaspining@gmail.com>
2024-05-20 18:39:24 +0300

    FIX | Cookie(domain)

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({ origin: process.env.CLIENT_BASE_URL, credentials: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
